6edf505ba209ceaf77b252aab1abda5aeaa4089d
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-04-04 21:53:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const wins = record.find(lookup);
+    
+    if(wins) {
+        return wins.year;
+    }
+    return undefined;
+}
+
+function lookup(record) {
+    return record.result === "W";
+}
